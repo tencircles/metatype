d7b55322dea3e6ccb6d4b47f8d0e514c75f7efba
werelax <eliasagc@gmail.com>
2014-03-28 10:48:02 +0100

    Adapted to my own coding style and able to read prototypes defined without defineProperty syntax

--- metatype.js
+++ metatype.js
@@ -7,32 +7,44 @@
     this.metatype = definition();
   }
 })(function () {
-    function init (obj, args) {
-        return (obj.init && obj.init.apply(obj, args)), obj;
-    }
-    function getter (obj, key) {
-        return (obj && obj[key]) ? obj[key] : void 0;
-    }
-    return {
-        extend: function extend () {
-            return [].reduce.call(arguments, Object.create, this);
-        },
-        create: function create () {
-            return init(Object.create(this), arguments);
-        },
-        get: function get (keys) {
-            return keys.split(/\./).reduce(getter, this);
-        },
-        set: function set (keysString, value) {
-            var keys = keysString.split(/\./),
-                obj = this,
-                key;
+  function init (obj, args) {
+    return (obj.init && obj.init.apply(obj, args)), obj;
+  }
+  function getter (obj, key) {
+    return (obj && obj[key]) ? obj[key] : void 0;
+  }
+  function injectValue(obj, key) {
+    var v = obj[key];
+    if (!v.value && !v.get)
+      Object.defineProperty(obj, key, { value: v, enumerable: true });
+  }
+  function injectValues(obj) {
+    for (var key in obj) injectValue(obj, key);
+    return obj;
+  }
+  function extender(obj, proto) {
+    return Object.create(obj, injectValues(proto));
+  }
+  return {
+    extend: function() {
+      return [].reduce.call(arguments, extender, this);
+    },
+    create: function() {
+      return init(Object.create(this), arguments);
+    },
+    get: function(keys) {
+      return keys.split(/\./).reduce(getter, this);
+    },
+    set: function(keysString, value) {
+      var keys = keysString.split(/\./),
+          obj = this,
+          key;
 
-            while (key = keys.shift(), keys.length) {
-                obj = typeof obj[key] !== "undefined" ? obj[key] :
-                    obj[key] = +keys[0] === +keys[0] ? [] : {};
-            }
-            return !keys.length ? obj[key] = value : null;
-        }
-    };
+      while (key = keys.shift(), keys.length) {
+        obj = typeof obj[key] !== "undefined" ? obj[key] :
+          obj[key] = isNaN(+keys[0]) ? {}: [];
+      }
+      return !keys.length ? obj[key] = value : null;
+    }
+  };
 });
